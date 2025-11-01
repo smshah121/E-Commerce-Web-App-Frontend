da34ecae89684e91fa007d5e6b244df1ccb48452
smshah121 <smshah.2003@gmail.com>
2025-11-01 12:42:19 +0500

    changes saved

--- src/component/customer/Navbar.jsx
+++ src/component/customer/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useGetAllProductsQuery } from '../../feature/product/productApi';
+import { motion} from "framer-motion";
 
 const Navbar = ({ isLoggedIn, role, onScrollToSection }) => { 
   const navigate = useNavigate();
