22692351a2f044c0a0b17cef07661548040745d2
Syed Momin Ali Shah <smshah.2003@gmail.com>
2026-07-18 21:43:39 +0500

    cs

--- src/component/customer/MyOrders.jsx
+++ src/component/customer/MyOrders.jsx
@@ -110,7 +110,7 @@ const MyOrders = () => {
   return (
     <div className="min-h-screen bg-gray-50">
       <CustomerNavbar /> {/* Include the Navbar */}
-      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-12 pt-20">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-12 pt-24">
         {" "}
         {/* Add pt-20 for navbar clearance */}
         <motion.button
