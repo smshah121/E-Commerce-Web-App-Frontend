d6d0d7cd4ff40a916a15700703f354883ec19954
Syed Momin Ali Shah <smshah.2003@gmail.com>
2026-07-29 00:23:57 +0500

    cs

--- src/component/customer/StorePage.jsx
+++ src/component/customer/StorePage.jsx
@@ -20,14 +20,15 @@ const StorePage = () => {
     <div className="min-h-screen bg-white">
 
       <section className="bg-black py-20 text-white">
-        
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <span 
-          className="px-4 mb-5 py-2 text-gray-400 hover:text-white cursor-pointer"
+        <span 
+          className="px-4 mb-10 py-2 text-gray-400 hover:text-white cursor-pointer"
           onClick={() => navigate("/")}
         >
           Back
         </span>
+        
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            
           <p className="text-sm font-semibold uppercase tracking-widest text-gray-400">
             Store
           </p>
