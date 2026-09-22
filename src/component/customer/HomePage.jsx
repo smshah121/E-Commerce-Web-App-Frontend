532d661b3028f9264da5c45f521787790ac535e1
Syed Momin Ali Shah <smshah.2003@gmail.com>
2026-09-22 16:19:12 +0500

    cs

--- src/component/customer/HomePage.jsx
+++ src/component/customer/HomePage.jsx
@@ -483,7 +483,7 @@ const HomePage = () => {
           Price
         </p>
         <span className="text-xl font-black text-slate-900">
-          {formatPrice(product.price)}
+         Rs. {formatPrice(product.price)}
         </span>
       </div>
 
