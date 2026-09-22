d516adff2fdae3f11132d9c9421d7b6a7e99d9c4
Syed Momin Ali Shah <smshah.2003@gmail.com>
2026-09-22 16:26:07 +0500

    cs

--- src/component/customer/HomePage.jsx
+++ src/component/customer/HomePage.jsx
@@ -41,10 +41,10 @@ const HomePage = () => {
   };
 
   const formatPrice = (price) => {
-    return typeof price === "number"
-      ? price.toFixed(2)
-      : parseFloat(price || 0).toFixed(2);
-  };
+  return Number(price || 0).toLocaleString("en-PK", {
+    maximumFractionDigits: 0,
+  });
+};
 
   const handleScrollToSection = (sectionId) => {
     const section = document.getElementById(sectionId);
